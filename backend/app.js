3ba03f583a973e28a7dbc0d6ee817b30059cc6ec
dkachurynets <dkachurynets@corevalue.net>
2019-09-30 19:02:02 +0300

    fetching initial posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello form express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id: 'fladjsflaj', title: 'First server-side posts', content: 'lfadjlfajlfdja'},
+    {id: 'flafasdfaddjsflaj', title: 'Second server-side posts', content: 'lfadjlfajlfdja'}
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
